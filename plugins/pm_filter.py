81be99f5972c115ba3fe3699c12001ebccd65b50
sheffykhlg <56907336+sheffykhlg@users.noreply.github.com>
2024-08-19 14:03:05 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2298,7 +2298,26 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
-            
+    elif query.data == "shortlink_info":
+            btn = [[
+                InlineKeyboardButton("👇Select Your Language 👇", callback_data="laninfo")
+        ], [
+            InlineKeyboardButton("English", callback_data="english_info")
+        ], [
+            InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start")
+
+            ]]
+            await client.edit_message_media(
+                query.message.chat.id, 
+                query.message.id, 
+                InputMediaPhoto(random.choice(PICS))
+            )
+            reply_markup = InlineKeyboardMarkup(btn)
+            await query.message.edit_text(
+                text=(script.SHORTLINK_INFO),
+                reply_markup=reply_markup,
+                parse_mode=enums.ParseMode.HTML
+            )    
     elif query.data == "tele":
             btn = [[
                     InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="help"),
