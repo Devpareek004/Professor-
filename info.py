a53fde17138483150898109edbe513f782180af0
Devpareek004 <165404806+Devpareek004@users.noreply.github.com>
2024-09-08 10:39:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,7 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+Xne-gvMOVKo4OGE1')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+swVCuJvmFykwODdl')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+XwzVvi5fd01iYTI1')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/howtoopnelink001/18')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '+KkiJxjEDnlU5MjJl') # Support Chat Link Without https:// or @
