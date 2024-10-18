30e8035b52eea599209b69f3b4ec8844df22fe4d
Devpareek004 <165404806+Devpareek004@users.noreply.github.com>
2024-10-18 12:56:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2719,9 +2719,9 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
                 cap += f"<b>\n📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n</a></b>"
     else:
         if settings["button"]:
-            cap = f"<b>Hᴇʏ.{message.from_user.mention},I Fᴏᴜɴᴅ Sᴏᴍᴇ Rᴇꜱᴜʟᴛꜱ Fᴏʀ Yᴏᴜʀ Qᴜᴇʀʏ ☞ {search}Rᴇǫᴜᴇsᴛᴇᴅ Bʏ ☞ {message.from_user.mention}ʀᴇsᴜʟᴛ sʜᴏᴡ ɪɴ ☞ {remaining_seconds} sᴇᴄᴏɴᴅsᴘᴏᴡᴇʀᴇᴅ ʙʏ ☞ : {message.chat.title}</b>"
+            cap = f"<b>Hᴇʏ.{message.from_user.mention},I Fᴏᴜɴᴅ Sᴏᴍᴇ Rᴇꜱᴜʟᴛꜱ Fᴏʀ Yᴏᴜʀ Qᴜᴇʀʏ ☞ {search}</b>"
         else:
-            cap = f"<b>Hᴇʏ.{message.from_user.mention},I Fᴏᴜɴᴅ Sᴏᴍᴇ Rᴇꜱᴜʟᴛꜱ Fᴏʀ Yᴏᴜʀ Qᴜᴇʀʏ ☞ {search}nRᴇǫᴜᴇsᴛᴇᴅ Bʏ ☞ {message.from_user.mention}ʀᴇsᴜʟᴛ sʜᴏᴡ ɪɴ ☞ {remaining_seconds} sᴇᴄᴏɴᴅsᴘᴏᴡᴇʀᴇᴅ ʙʏ ☞ : {message.chat.title}</b>"
+            cap = f"<b>Hᴇʏ.{message.from_user.mention},I Fᴏᴜɴᴅ Sᴏᴍᴇ Rᴇꜱᴜʟᴛꜱ Fᴏʀ Yᴏᴜʀ Qᴜᴇʀʏ ☞ {search}</b>"
             cap+="<b><u>🍿 Your Movie Files 👇</u></b>\n\n"
             for file in files:
                 cap += f"<b>📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
